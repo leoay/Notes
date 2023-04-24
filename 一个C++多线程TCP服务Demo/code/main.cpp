9e197881fe11a77776651229317a282373b865d3
leoay <leoay23@hotmail.com>
2023-04-24 08:03:54 +0800

    first commit

---
+++ 一个C++多线程TCP服务Demo/code/main.cpp
@@ -0,0 +1,11 @@
+#include "TCPServer.h"
+
+int main() 
+{
+    // create server - it starts automatically in constructor
+    TCPServer server;
+    // server.stop(); // we could stop the server this way
+    // wait for server thread to end
+    server.join();
+    return 0;
+}
